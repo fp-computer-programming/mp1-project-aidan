2d315630db0a975f1e811d286fec47198774ef98
p23aelliff <p23aelliff@fairfieldprep.org>
2022-10-31 09:39:52 -0400

    end of quarter project

---
+++ MP1_Project.py
@@ -0,0 +1,244 @@
+#author ate (10/24/22)
+
+#words needed to complete Mad Lib
+word1 = "Azpeitia"
+word2 = "Spain"
+word3 = "gambler"
+word4 = "women"
+word5 = "swordplay"
+word6 = "16"
+word7 = "page"
+word8 = "treasurer"
+word9 = "30"
+word10 = "battle"
+word11 = "French"
+word12 = "officer"
+word13 = "Fortress of Pamplona" 
+word14 = "cannonball"
+word15 = "brought"
+word16 = "Castle of Loyola"
+word17 = "Christianity"
+word18 = "recovery"
+word19 = "the life of Christ"
+word20 = "the Saints"
+word21 = "Roman Catholic"
+word22 = "colleagues"
+word23 = "Francis Xavier"
+word24 = "Peter Fabier"
+word25 = "Universty of Paris"
+word26 = "France"
+word27 = "The Jesuit Order"
+word28 = "miracles"
+word29 = "letters"
+word30 = "constitutions"
+word31 = "death"
+word32 = "July 31, 1609"
+word33 = "feast"
+
+#elif statements
+if word1 == "Azpeitia":
+    print(word1)
+elif word1 != "Apzeitia":
+    print("Please chose a different city")
+else:
+     print("Try again")
+if word2 == "Spain":
+    print(word2)
+elif word2 != "Spain":
+    print("Please chose a different location")
+else:
+    print("Try again")
+if word3 == "gambler":
+    print(word3)
+elif word3 != "gambler":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word4 == "women":
+    print(word4)
+elif word4 != "women":
+    print("Please chose a different noun")
+else:
+    print("Try again")
+if word5 == "swordplay":
+    print(word5)
+elif word5 != "swordplay":
+    print("Please chose a different noun")
+else:
+    print("Try again")
+if word6 == "16":
+    print(word6)
+elif word6 != "16":
+    print("Please chose a different age")
+else:
+    print("Try again")
+if word7 == "page":
+    print(word7)
+elif word7 != "page":
+    print("Please chose a different noun")
+else:
+    print("Try again")
+if word8 == "treasurer":
+    print(word8)
+elif word8 != "treasurer":
+    print("Please chose a different noun")
+else:
+    print("Try again")
+if word9 == "30":
+    print(word9)
+elif word9 != "30":
+    print("Please chose a different age")
+else:
+    print("Try again")
+if word10 == "battle":
+    print(word10)
+elif word10 != "battle":
+    print("Please chose a different noun")
+else:
+    print("Try again")
+if word11 == "French":
+    print(word11)
+elif word11 != "French":
+    print("Please chose a different group")
+else:
+    print("Try again")
+if word12 == "officer":
+    print(word12)
+elif word12 != "officer":
+    print("Please chose a different noun")
+else:
+    print("Try again")
+if word13 == "Fortress of Pamplona":
+    print(word13)
+elif word13 != "Fortress of Pamplona":
+    print("Please chose a different location")
+else:
+    print("Try again")
+if word14 == "cannonball":
+    print(word14)
+elif word14 != "cannonball":
+    print("Please chose a different noun")
+else:
+    print("Try again")
+if word15 == "brought":
+    print(word15)
+elif word15 != "brought":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word16 == "Castle of Loyola":
+    print(word16)
+elif word16 != "Castle of Loyola":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word17 == "Christianity":
+    print(word17)
+elif word17 != "Christianity":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word18 == "recovery":
+    print(word18)
+elif word18 != "recovery":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word19 == "the life of Christ":
+    print(word19)
+elif word19 != "the life of Christ":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word20 == "the Saints":
+    print(word20)
+elif word20 != "the Saints":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word21 == "Roman Catholic":
+    print(word21)
+elif word21 != "Roman Catholic":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word22 == "colleagues":
+    print(word22)
+elif word22 != "colleagues":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word23 == "Francis Xavier":
+    print(word23)
+elif word23 != "Francis Xavier":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word24 == "Peter Fabier":
+    print(word24)
+elif word24 != "Peter Fabier":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word25 == "Universty of Paris":
+    print(word25)
+elif word25 != "Universty of Paris":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word26 == "France":
+    print(word26)
+elif word26 != "France":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word27 == "The Jesuit Order":
+    print(word27)
+elif word27 != "The Jesuit Order":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word28 == "miracles":
+    print(word28)
+elif word28 != "miracles":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word29 == "letters":
+    print(word29)
+elif word29 != "letters":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word30 == "constitutions":
+    print(word30)
+elif word30 != "constitutions":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word31 == "death":
+    print(word31)
+elif word31 != "death":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word32 == "July 31, 1609":
+    print(word32)
+elif word32 != "July 31, 1609":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+if word33 == "feast":
+    print(word33)
+elif word33 != "feast":
+    print("Please chose a different verb")
+else:
+    print("Try again")
+
+#Mad Lib paragraph and print statement
+line1 = "In 1491, St. Ignatius, was born in {0}, in the Basque province of Guipuzcoa of northern {1}. Through his early life, he was a very big {2}, a great desire for {3}, and avid fan of {4}. Ignatius was sent at {5} years, to work as a {6} for Juan Velazquez. Whom held the title of {7} of the kingdom of Castile. By age of {8}, Ignatius was found at the {9} of Pamplona, fighting the {10}".format(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10, word11)
+line2 = "Where he was a great {0} defending the {1}. He inevitably won the battle against the French. The group found Ingatius honorable for taking a {2} to the leg. In turn, they had {3} Ignatius back to his home, the {4}, rather than to prison. During his recuperation, Ignatius discovered {5} through reading. During his {6}, he started reading books about {7} and {8}".format(word12, word13, word14, word15, word16, word17, word18, word19, word20)
+line3 = "These books later caused him to convert to {0}. Through his journey in the roman catholic faith, he had met two of his closest {1}, {2} and {3}. This was caused by the three meeting at a school, called the {4}, in {5}. Ignatius later founded the {6}, which had grown greatly".format(word21, word22, word23, word24, word25, word26, word27)
+line4 = "Throughout his life after creating the Order, St. Ignatius has performed 160 {0}. He had wrote thousands of {1}, {2} for the Society, till his {3} in 1609. Since his death, the Church had made {4}, to be a {5} in his honor".format(word28, word29, word30, word31, word32, word33)
+
+print(line1 + line2 + line3 + line4)
